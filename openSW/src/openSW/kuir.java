77c6f5ca071615e323f5348073814a9d90688175
kyuhyeongkwon <akcps1tp@naver.com>
2021-03-18 19:09:23 +0900

    kuir.java 파일 수정

--- openSW/src/openSW/kuir.java
+++ openSW/src/openSW/kuir.java
@@ -5,7 +5,6 @@ public class kuir {
 	public static void main(String[] args) {
 		
 		if (args[0].equals("-c")) {
-			System.out.println(args[1]);
 			makeCollection mc = new makeCollection(args[1]);
 		}
 		else if (args[0].equals("-k")) {
