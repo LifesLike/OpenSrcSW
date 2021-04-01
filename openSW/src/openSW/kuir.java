b98eaa1ecad229689d48278430d19264991c4d5e
kyuhyeongkwon <akcps1tp@naver.com>
2021-04-01 15:01:58 +0900

    kuir.java 업데이트

--- openSW/src/openSW/kuir.java
+++ openSW/src/openSW/kuir.java
@@ -4,22 +4,25 @@ public class kuir {
 
 	public static void main(String[] args) {
 		
-		long beforeTime = System.nanoTime();
+		
 		
 		if (args[0].equals("-c")) {
 			makeCollection mc = new makeCollection(args[1]);
 		}
+		
 		else if (args[0].equals("-k")) {
 			makeKeyword mk = new makeKeyword(args[1]);
 		}
+		
 		else if (args[0].equals("-i")) {
 			indexer ixr = new indexer(args[1]);
-
 		}
 		
-		long afterTime = System.nanoTime();
-		long diff = (afterTime - beforeTime);
-		System.out.println(diff);
+		else if (args[0].equals("-s")) {
+			searcher sc = new searcher(args[1], 5);
+			
+		}
+		
 	}
 
 }
